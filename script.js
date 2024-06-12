a9df742e433d1336936ec6adaacfe40aae963bdc
timsilve <timsilva112.0@gmail.com>
2024-06-12 05:33:43 +0100

    updated

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 let num = document.querySelectorAll(".box");
 let number = document.querySelector("#number");
-let red = document.querySelectorAll(".red")
+let clear = document.getElementById("mark");
 
-let score = 3;
 num.forEach((user) => {
   user.addEventListener("click", () => {
     user.classList.remove("hidden");
 
-//  red.classList.remove("red")
+    let redElement = user.querySelector(".red");
+    if (redElement) {
+      redElement.classList.remove("red");
+    }
 
-red.forEach(event => {
-    event.classList.remove("red")
-})
-     
- 
- if (number.textContent > 0) {
+    if (number.textContent > 0) {
       number.textContent -= 1;
     } else {
       number.textContent = 0;
-     }
+    }
   });
+});
 
-  
+clear.addEventListener("click", function () {
+  num.forEach((user) => {
+    user.classList.remove("hidden");
   });
-
+  document.querySelectorAll(".red").forEach((reds) => {
+    reds.classList.remove("red");
+  });
+  number.textContent = 0
+});
